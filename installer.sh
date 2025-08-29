84337fd1a2bcf900a68d630a4b3dad7ae78c25c8
RafaelLdaSilva <rlslink@hotmail.com>
2025-08-28 21:17:12 -0300

    + melhorias no installer.sh, removido log do .openpipes_cache pois continha informações proprieatárias de terceiros...

--- installer.sh
+++ installer.sh
@@ -11,21 +11,21 @@ config_file="$HOME/.openpipes/config.sh"
 echo $path
 
 echo "##############################################################"
-echo "  Copying .openpipes folder to your \$HOME ... "
+echo " Copying .openpipes folder to your \$HOME ... "
 echo "##############################################################"
 
 cp -r .openpipes/ $HOME
 cp -r .openpipes_cache/ $HOME
 
 echo "##############################################################"
-echo "  Copying scripts to folder $path ... "
-echo "  Please enter sudo credentials ... "
+echo " Copying scripts to folder $path ... "
+echo " Please enter sudo credentials ... "
 echo "##############################################################"
 
 sudo cp $scripts_dir/* $path
 
 echo "##############################################################"
-echo "  Making scripts executable (chmod +x) ... "
+echo " Making scripts executable (chmod +x) ... "
 echo "##############################################################"
 
 for file in $(ls $scripts_dir/* | rev | cut -d "/" -f1 | rev);do 
@@ -33,55 +33,80 @@ for file in $(ls $scripts_dir/* | rev | cut -d "/" -f1 | rev);do
 done
 
 echo "##############################################################"
-echo "  Downgrading amass to version 3.20.0 ... "
+echo " Downgrading amass to version 3.20.0 ... "
 echo "##############################################################"
 
 wget $amass_ver && unzip amass_linux_amd64.zip && sudo cp amass_linux_amd64/amass $amass_path
 
 echo "##############################################################"
-echo "  Downgrading dnsrecon to version 1.1.3 ... "
+echo " Downgrading dnsrecon to version 1.1.3 ... "
 echo "##############################################################"
 
 wget $dnsrecon_ver && unzip 1.1.3.zip && sudo rm -rf $dnsrecon_dir/* && sudo mkdir -p $dnsrecon_dir && sudo cp -r dnsrecon-1.1.3/* $dnsrecon_dir
 
 echo "##############################################################"
-echo "  Updating repositories and installing RDAP ... "
+echo " Updating repositories and installing RDAP ... "
 echo "##############################################################"
 
 sudo apt-get update && sudo apt install rdap -y
 
 echo "##############################################################"
-echo "  Populating local cache with top 10 vulnerabilities  ... "
+echo " Populating local cache with top 10 vulnerabilities  ... "
 echo "##############################################################"
 
 python3 .openpipes/populate-cache-json.py
 
 echo "##############################################################"
-echo "  Enter your Security Trails API Key ... "
+echo " Enter your Security Trails API Key ... "
 echo "##############################################################"
 
 read -p "You can set your API Keys later in $HOME/.openpipes/config.sh: " sectrailskey
 sed -i 's/securitytrailskey=.*/securitytrailskey='\""${sectrailskey}"\"'/' $config_file
 
 echo "##############################################################"
-echo "  Enter your Open AI API Key ... "
+echo " Enter your Open AI API Key ... "
 echo "##############################################################"
 
 read -p "You can set your API Keys later in $HOME/.openpipes/config.sh: " OPENAI_API_KEY
 sed -i 's/OPENAI_API_KEY=.*/OPENAI_API_KEY='\""${OPENAI_API_KEY}"\"'/' $config_file
 
 echo "##############################################################"
-echo "  Enter the directory path to your Projects directory ... "
+echo " Enter the directory path to your Projects directory ... "
 echo "##############################################################"
 
 read -p "Directory to host all your openPipes Projects: " proj_dir
 sed -i 's-proj_dir=.*-proj_dir='\""${proj_dir}"\"'/' $config_file
 
 echo "##############################################################"
-echo "  Enter the name of your first Project ... "
+echo " Enter the name of your first Project ... "
 echo "##############################################################"
 
 read -p "Name of your first Project (usually your target's business name): " proj_name
 sed -i 's-proj_name=.*-proj_name='\""${proj_name}"\"'-' $config_file
+
+echo "##############################################################"
+echo " Enter domains one at a time you wish OPenPipeS to Recon ... "
+echo " Obs.: Please note you need ONLY 2nd level domains, ex.: "
+echo " example.com "
+echo " tesla.com "
+echo " terra.com.br "
+echo " etc... "
+echo "##############################################################"
+
+proj_path=${proj_dir}/${proj_name}
+
+read -p "Enter a domain to start the Reconnaissance: " domain
+echo $domain > ${proj_path}/domains.txt
+
+echo "++ Recon file ${proj_path}/domains.txt created, add as many domains as you want for the Recon phase."
+
+echo "##############################################################"
+echo " Avoid using 3rd+ level domains (ex. sub.example.com) as "
+echo " this will significantly diminish the mapped attack surface "
+echo "##############################################################"
+
+while [[ -n $domain ]]; do
+    read -p "Enter another domain or press [ENTER] twice when you're done: " domain
+done
 
 # dependencies=("")
